aa2d95aeb959659a6a5735471ab599f3d57fdb26
Arturs Jansons <arturs@Arturss-MacBook-Pro.local>
2021-03-25 21:41:12 +0200

    Https group scheme

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,13 @@ use App\Http\Controllers\Auth\UserAuthController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', [UserAuthController::class, 'login']);
-Route::get('/login', [UserAuthController::class, 'login'])->name('login')->middleware('checkAuth');
-Route::group(['scheme' => 'https'], function () {
+Route::group(['scheme' => $_ENV["URL_SCHEME"]], function () {
     // Route::get(...)->name(...);
     // Route::get('/', [UserAuthController::class, 'login'])->middleware('redirectIfAuthenticated');
     // Route::get('/login', [UserAuthController::class, 'login'])->middleware('redirectIfAuthenticated');
     // Route::get('/register', [UserAuthController::class, 'register'])->middleware('redirectIfAuth');
+    Route::get('/', [UserAuthController::class, 'login']);
+    Route::get('/login', [UserAuthController::class, 'login'])->name('login')->middleware('checkAuth');
     Route::post('/create', [UserAuthController::class, 'createUser'])->name('auth.create');
     // Route::post('/check', [UserAuthController::class, 'loginUser'])->name('auth.check');
     Route::post('/check', [UserAuthController::class, 'authenticate'])->name('auth.check');
@@ -31,21 +31,23 @@ Route::group(['scheme' => 'https'], function () {
 });
 
 
-Route::middleware(['auth'])->group(function () {
-    Route::any('/{any}', [AngularController::class, 'index'])->where(
-        ['any', '^(?!api).*$'],
-        ['any', '^(?!web).*$']
-    );
-    Route::get('web/getLoggedUser', [UserAuthController::class, 'getLoggedUser']);
-
-    Route::get('web/getCities', [CityController::class, 'getCities']);
-    Route::post('web/updateOrCreateCity', [CityController::class, 'updateOrCreateCity']);
-    Route::post('web/deleteCity', [CityController::class, 'deleteCity']);
-
-    Route::get('web/getClients', [ClientController::class, 'getClients']);
-    Route::post('web/createClient', [ClientController::class, 'createClient']);
-    Route::post('web/updateClientName', [ClientController::class, 'updateClientName']);
-    Route::post('web/updateClientToken', [ClientController::class, 'updateClientToken']);
-    Route::get('web/getClientsToken', [ClientController::class, 'getClientsToken']);
+Route::group(['scheme' => $_ENV["URL_SCHEME"]], function () {
+    Route::middleware(['auth'])->group(function () {
+        Route::any('/{any}', [AngularController::class, 'index'])->where(
+            ['any', '^(?!api).*$'],
+            ['any', '^(?!web).*$']
+        );
+        Route::get('web/getLoggedUser', [UserAuthController::class, 'getLoggedUser']);
+    
+        Route::get('web/getCities', [CityController::class, 'getCities']);
+        Route::post('web/updateOrCreateCity', [CityController::class, 'updateOrCreateCity']);
+        Route::post('web/deleteCity', [CityController::class, 'deleteCity']);
+    
+        Route::get('web/getClients', [ClientController::class, 'getClients']);
+        Route::post('web/createClient', [ClientController::class, 'createClient']);
+        Route::post('web/updateClientName', [ClientController::class, 'updateClientName']);
+        Route::post('web/updateClientToken', [ClientController::class, 'updateClientToken']);
+        Route::get('web/getClientsToken', [ClientController::class, 'getClientsToken']);
+    });
 });
 
